834589096844d001115e15eb281b4c32d28c275e
pthatn456-bit <pthatn456@gmail.com>
2026-07-10 21:51:12 +0700

    k

--- apps/web/src/components/DownloadButton/DownloadButton.tsx
+++ apps/web/src/components/DownloadButton/DownloadButton.tsx
@@ -9,7 +9,9 @@ export default function DownloadButton(){
 
 const audioUrl =
 useTranslateStore(
+
 state=>state.audioUrl
+
 );
 
 
@@ -17,50 +19,110 @@ state=>state.audioUrl
 function download(){
 
 
+if(!audioUrl){
+
+return;
+
+}
+
+
+
 const link =
 document.createElement(
+
 "a"
+
 );
 
 
+
 link.href =
+
+"http://localhost:8000"
+
++
+
 audioUrl;
 
 
+
 link.download =
+
 "vietnamese.mp3";
 
 
+
+document.body.appendChild(
+
+link
+
+);
+
+
+
 link.click();
+
+
+
+link.remove();
 
 
 }
 
 
 
+
 return(
+
 
 <button
 
 onClick={download}
 
+disabled={!audioUrl}
+
 style={{
 
 padding:"12px 20px",
 
-background:"#16a34a",
+background:
+
+audioUrl
+
+?
+
+"#16a34a"
+
+:
+
+"#9ca3af",
 
 color:"white",
 
-borderRadius:8
+borderRadius:8,
+
+cursor:
+
+audioUrl
+
+?
+
+"pointer"
+
+:
+
+"not-allowed"
 
 }}
 
 >
 
+
 Download Vietnamese MP3
 
+
 </button>
+
 
 );
 
