99f8e25a702766d9a5a8aeed9a6c4684516ac13f
pthatn456-bit <pthatn456@gmail.com>
2026-07-10 22:56:18 +0700

    k

--- apps/web/src/pages/Home/Home.tsx
+++ apps/web/src/pages/Home/Home.tsx
@@ -1,62 +1,21 @@
-import { useState } from "react";
-import { uploadMp3 } from "../../services/upload.service";
-import { translate } from "../../services/translate.service";
-import AudioUpload
-from "../../components/AudioUpload/AudioUpload";
-
-
-import TranslateProgress
-from "../../components/TranslateProgress/TranslateProgress";
-
-
-import AudioResult
-from "../../components/AudioResult/AudioResult";
-
-
+import AudioUpload from "../../components/AudioUpload/AudioUpload";
+import TranslateProgress from "../../components/TranslateProgress/TranslateProgress";
+import AudioResult from "../../components/AudioResult/AudioResult";
 
 export default function Home() {
   return (
-    <div style={{ padding: 40 }}>
-      <h1 style={{ color: "red" }}>
-        HOME ĐANG CHẠY
-      </h1>
+    <div className="container">
+      <h1>YouTube Voice Translator</h1>
+
+      <AudioUpload />
+
+      <br />
+
+      <TranslateProgress />
+
+      <br />
+
+      <AudioResult />
     </div>
   );
-}
-
-return (
-
-<div className="container">
-
-
-<h1>
-
-YouTube Voice Translator
-
-</h1>
-
-
-<br/>
-
-
-<AudioUpload onSelect={handleUpload} />
-
-
-<br/>
-
-
-<TranslateProgress/>
-
-
-<br/>
-
-
-<AudioResult url={downloadUrl} />
-
-
-</div>
-
-);
-
-
 }
