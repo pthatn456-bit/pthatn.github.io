c3898af36f09e5fe380131c003388cdcfbbb6dcb
pthatn456-bit <pthatn456@gmail.com>
2026-07-11 15:16:31 +0700

    k

--- apps/web/src/components/AudioUpload/AudioUpload.tsx
+++ apps/web/src/components/AudioUpload/AudioUpload.tsx
@@ -39,80 +39,39 @@ useTranslate(jobId);
 
 
 
+
 async function submit(){
 
     if(!file){
+        alert("Vui lòng chọn file MP3");
         return;
     }
 
+    try {
 
-    const result =
-    await uploadAudio(
-        file
-    );
+        const result = await uploadAudio(file);
 
+        console.log("Upload result:", result);
 
-    setJobId(
-        result.job_id
-    );
+        setJobId(
+            result.job_id
+        );
 
+        setProgress(
+            10
+        );
 
-    setProgress(
-        10
-    );
+    } catch(error){
 
-}
+        console.error(
+            "Upload failed:",
+            error
+        );
 
+        alert(
+            "Không thể tải file lên server"
+        );
 
-
-return (
-
-<div className="card">
-
-
-<h2>
-    Chọn MP3 tiếng Anh
-</h2>
-
-
-<input
-
-type="file"
-
-accept=".mp3"
-
-onChange={
-
-(e)=>
-
-setFile(
-
-e.target.files?.[0] || null
-
-)
-
-}
-
-/>
-
-
-<br/><br/>
-
-
-<button
-
-onClick={submit}
-
->
-
-Dịch sang MP3 tiếng Việt
-
-</button>
-
-
-</div>
-
-);
-
+    }
 
 }
