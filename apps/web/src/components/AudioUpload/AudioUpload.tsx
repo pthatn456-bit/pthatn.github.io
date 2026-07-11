0c9d7080130392e850637c85de31a375d400f464
pthatn456-bit <pthatn456@gmail.com>
2026-07-11 15:27:04 +0700

    k

--- apps/web/src/components/AudioUpload/AudioUpload.tsx
+++ apps/web/src/components/AudioUpload/AudioUpload.tsx
@@ -75,3 +75,46 @@ async function submit(){
     }
 
 }
+}
+
+
+return (
+
+<div className="card">
+
+<h2>
+    Chọn MP3 tiếng Anh
+</h2>
+
+
+<input
+
+type="file"
+
+accept=".mp3"
+
+onChange={(e)=>
+    setFile(
+        e.target.files?.[0] || null
+    )
+}
+
+/>
+
+
+<br/><br/>
+
+
+<button
+onClick={submit}
+>
+    Dịch sang MP3 tiếng Việt
+</button>
+
+
+</div>
+
+);
+
+
+}
